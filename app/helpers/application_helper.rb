e8fa03f1514033a95355940486f077612241d7a6
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-02 22:24:41 +0100

    Added some style tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Pinteresting"
+    base_title = "Ineke Vosman"
     if page_title.empty?
       base_title
     else
